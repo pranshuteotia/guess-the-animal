283ee3a59d52d740093be2820a360891d65ef3aa
Pranshu Teotia <pranshu28@gmail.com>
2025-07-04 04:08:23 +0530

    feat: add favicon

--- src/hooks/use-image.ts
+++ src/hooks/use-image.ts
@@ -5,12 +5,18 @@ export const useImage = (fileName: string) => {
   const [error, setError] = useState(null);
   const [image, setImage] = useState(null);
 
+  // const images = import.meta.glob('./images/*.jpg', { eager: true });
+
   useEffect(() => {
     const fetchImage = async () => {
       setLoading(true);
       try {
-        const response = await import(`./images/${fileName}`);
-        setImage(response.default);
+        const response = import.meta.glob(`./images/*.webp`, {
+          eager: true,
+        });
+        const imageUrl = (response[fileName] as any)?.default;
+        response;
+        setImage(imageUrl);
       } catch (err: any) {
         setError(err);
       } finally {
